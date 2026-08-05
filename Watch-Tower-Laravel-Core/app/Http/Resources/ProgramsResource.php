e3fc1b951961af8c271475d0195bd1ad78084cd0
Arash-abraham <arashebi777@gmail.com>
2026-08-05 19:31:18 +0330

    chore: add Program API structure
    - Create ProgramController with API methods
    - Add ProgramRequest for validation
    - Add ProgramResource for response transformation

--- Watch-Tower-Laravel-Core/app/Http/Resources/ProgramsResource.php
+++ Watch-Tower-Laravel-Core/app/Http/Resources/ProgramsResource.php
@@ -14,6 +14,6 @@ class ProgramsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request); //
+        return parent::toArray($request);
     }
 }
